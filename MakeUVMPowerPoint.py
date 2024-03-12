65732cffa42fc0e842eac600534179234ce72dc8
chungkee <chungkeechua@gmail.com>
2024-03-13 00:00:39 +0800

    Complete all offset and testing of UVM powerpoint, maximun 4 agent in ppt

--- MakeUVMPowerPoint.py
+++ MakeUVMPowerPoint.py
@@ -4,9 +4,19 @@ from pptx.enum.shapes import MSO_SHAPE
 from pptx.enum.text import PP_ALIGN, MSO_VERTICAL_ANCHOR
 from pptx.dml.color import RGBColor
 
+class UVMNode:
+    def __init__(self, name, child, type) -> None:
+        self.name = name
+        self.child = child
+        self.type = type
+
+
+
 class MakeUVMPowerPoint:
     def __init__(self) -> None:
         self.prs = Presentation()
+        self.prs.slide_width = 16 * 914400  
+        self.prs.slide_height = 9 * 914400
         self.slide = None
         self.slide_layout = None
 
@@ -38,24 +48,55 @@ class MakeUVMPowerPoint:
     def SavePowerPoint(self):
         # Save the presentation
         self.prs.save("my_presentation.pptx")
+
+    def CreateTest(self, x = 0.1, y = 0.1, NumberAgent = 1):
+        self.CreateComponent(x = x, y = y, width = 0.075 + 0.225 * NumberAgent, height = 0.5, name = "Test",
+                             shape = MSO_SHAPE.RECTANGLE,
+                             P_alignment = PP_ALIGN.LEFT,
+                             color = RGBColor(112, 48, 160))
+        return None
+    def CreateEnv(self, x = 0.1, y = 0.1, NumberAgent = 1):
+        self.CreateComponent(x = x + 0.025, y = y + 0.05, width = 0.025 + 0.225 * NumberAgent, height = 0.4, name = "Env",
+                             shape = MSO_SHAPE.RECTANGLE,
+                             P_alignment = PP_ALIGN.LEFT,
+                             color = RGBColor(0,112,192))
+        return None
     
+    def CreateAgent(self, x = 0.1, y = 0.1, NumberAgent = 1):
+        for i in range(NumberAgent):
+            self.CreateComponent(x = x + 0.05 + i * 0.225, y = y + 0.2, width = 0.2, height = 0.2, name = "Agent1",
+                                shape = MSO_SHAPE.RECTANGLE,
+                                P_alignment = PP_ALIGN.LEFT,
+                                color = RGBColor(0, 176, 240))
+            self.CreateComponent(x = x + 0.075 + i * 0.225, y = y + 0.3, width = 0.07, height = 0.05, name = "Driver")
+            self.CreateComponent(x = x + 0.165 + i * 0.225, y = y + 0.3, width = 0.07, height = 0.05, name = "Monitor")
     def Run(self):
         self.CreateSlide()
-        self.CreateComponent(x = 0.1, y = 0.1, width = 0.3, height = 0.2, name = "WriteAgent",
-                             shape = MSO_SHAPE.RECTANGLE,
-                             P_alignment = PP_ALIGN.LEFT)
+        NumberAgent = 2
+        x = 0.5 - (0.075 + 0.225 * NumberAgent)/2
+        y = 0.2
+        self.CreateTest(x, y, NumberAgent = NumberAgent)
+        self.CreateEnv(x,y, NumberAgent = NumberAgent)
+        self.CreateComponent(x =  0.45, y = y + 0.1, width = 0.1, height = 0.05, name = "Scoreboard",
+                             color = RGBColor(0, 176, 240))
+        self.CreateAgent(x,y,NumberAgent = NumberAgent)
 
-        self.CreateComponent(x = 0.1, y = 0.7, width = 0.12, height = 0.05, name = "Driver")
-        self.CreateComponent(x = 0.3, y = 0.7, width = 0.12, height = 0.05, name = "Monitor")
-
-        self.CreateComponent(x = 0.425, y = 0.3, width = 0.15, height = 0.05, name = "Scoreboard")
         self.SavePowerPoint()
 
 if __name__ == "__main__":
     
     MUP = MakeUVMPowerPoint()
     MUP.Run()
-
+    test_name     = "AsynFIFO"
+    monitor       = UVMNode("monitor",       child = None,               type = "monitor")
+    driver        = UVMNode("driver",        child = None,               type = "driver")
+    scoreboard    = UVMNode("scoreboard",    child = None,               type = "scoreboard")
+    agent         = UVMNode("agent",         child = [driver,monitor],   type = "agent")
+    env           = UVMNode("env",           child = [agent,scoreboard], type = "env")
+    sequence_item = UVMNode("sequence_item", child = None,               type = "sequence_item")
+    sequence      = UVMNode("sequence",      child = [sequence_item],    type = "sequence")
+    test          = UVMNode("test",          child = [env,sequence],     type = "test")
+    
 #%%
 
 
